4fb2bd79390004a55f15cddf0672d9d77a14fa42
lavia <ty.zhao@live.com>
2023-05-29 20:27:08 +0800

    14, 3

---
+++ src/main/java/com/tex1/leetcode/editor/cn/CheckCompletenessOfABinaryTree.java
@@ -0,0 +1,31 @@
+//package com.tex1.leetcode.editor.cn;
+//
+//public class CheckCompletenessOfABinaryTree{
+//    public static void main(String[] args) {
+//        Solution solution = new CheckCompletenessOfABinaryTree().new Solution();
+//        //test here...
+//    }
+//    //leetcode submit region begin(Prohibit modification and deletion)
+///**
+// * Definition for a binary tree node.
+// * public class TreeNode {
+// *     int val;
+// *     TreeNode left;
+// *     TreeNode right;
+// *     TreeNode() {}
+// *     TreeNode(int val) { this.val = val; }
+// *     TreeNode(int val, TreeNode left, TreeNode right) {
+// *         this.val = val;
+// *         this.left = left;
+// *         this.right = right;
+// *     }
+// * }
+// */
+//class Solution {
+//    public boolean isCompleteTree(TreeNode root) {
+//
+//    }
+//}
+////leetcode submit region end(Prohibit modification and deletion)
+//
+//}
---
+++ src/main/java/com/tex1/leetcode/editor/cn/RotateList.java
@@ -0,0 +1,26 @@
+//package com.tex1.leetcode.editor.cn;
+//
+//public class RotateList{
+//    public static void main(String[] args) {
+//        Solution solution = new RotateList().new Solution();
+//        //test here...
+//    }
+//    //leetcode submit region begin(Prohibit modification and deletion)
+///**
+// * Definition for singly-linked list.
+// * public class ListNode {
+// *     int val;
+// *     ListNode next;
+// *     ListNode() {}
+// *     ListNode(int val) { this.val = val; }
+// *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+// * }
+// */
+//class Solution {
+//    public ListNode rotateRight(ListNode head, int k) {
+//
+//    }
+//}
+////leetcode submit region end(Prohibit modification and deletion)
+//
+//}
